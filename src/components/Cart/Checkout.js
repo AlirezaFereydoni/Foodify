8bb1ad11e48b86431eb714b12c89b635664ffbc2
AlirezaSoltanifar <a.soltanifar98@gmail.com>
2022-01-02 23:52:28 +0330

    Initial commit

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,140 @@
+import useInput from '../../hooks/use-input';
+
+import classes from './Checkout.module.css';
+
+const isEmpty = (value) => value.trim() !== '';
+const isFiveChars = (value) => value.trim().length === 5;
+
+const Checkout = (props) => {
+  const {
+    value: nameValue,
+    isValid: nameIsValid,
+    hasError: nameHasError,
+    valueChangeHandler: nameChangeHandler,
+    valueBlurHandler: nameBlurHandler,
+    reset: resetname,
+  } = useInput(isEmpty);
+
+  const {
+    value: streetValue,
+    isValid: streetIsValid,
+    hasError: streetHasError,
+    valueChangeHandler: streetChangeHandler,
+    valueBlurHandler: streetBlurHandler,
+    reset: resetstreet,
+  } = useInput(isEmpty);
+
+  const {
+    value: postalValue,
+    isValid: postalIsValid,
+    hasError: postalHasError,
+    valueChangeHandler: postalChangeHandler,
+    valueBlurHandler: postalBlurHandler,
+    reset: resetpostal,
+  } = useInput(isFiveChars);
+
+  const {
+    value: cityValue,
+    isValid: cityIsValid,
+    hasError: cityHasError,
+    valueChangeHandler: cityChangeHandler,
+    valueBlurHandler: cityBlurHandler,
+    reset: resetcity,
+  } = useInput(isEmpty);
+
+  const formIsValid = nameIsValid && streetIsValid && postalIsValid && cityIsValid;
+
+  const confirmHandler = (event) => {
+    event.preventDefault();
+
+    if (!formIsValid) {
+      return;
+    }
+
+    props.onConfirm({
+      name: nameValue,
+      street: streetValue,
+      city: cityValue,
+      postalCode: postalValue,
+    });
+
+    resetname();
+    resetstreet();
+    resetpostal();
+    resetcity();
+  };
+
+  const nameClasses = `${classes.control} ${nameHasError ? classes.invalid : ''}`;
+  const streetClasses = `${classes.control} ${streetHasError ? classes.invalid : ''}`;
+  const postalClasses = `${classes.control} ${postalHasError ? classes.invalid : ''}`;
+  const cityClasses = `${classes.control} ${cityHasError ? classes.invalid : ''}`;
+
+  return (
+    <form className={classes.form} onSubmit={confirmHandler}>
+      <div className={nameClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          value={nameValue}
+          onChange={nameChangeHandler}
+          onBlur={nameBlurHandler}
+        />
+        {nameHasError && (
+          <p className={classes.invalidtext}>Please enter a valid name!</p>
+        )}
+      </div>
+      <div className={streetClasses}>
+        <label htmlFor="street">Street</label>
+        <input
+          type="text"
+          id="street"
+          value={streetValue}
+          onChange={streetChangeHandler}
+          onBlur={streetBlurHandler}
+        />
+        {streetHasError && (
+          <p className={classes.invalidtext}>Please enter a valid street!</p>
+        )}
+      </div>
+      <div className={postalClasses}>
+        <label htmlFor="postal">Postal Code</label>
+        <input
+          type="text"
+          id="postal"
+          value={postalValue}
+          onChange={postalChangeHandler}
+          onBlur={postalBlurHandler}
+        />
+        {postalHasError && (
+          <p className={classes.invalidtext}>
+            Please enter a valid postal code (5 characters long)!
+          </p>
+        )}
+      </div>
+      <div className={cityClasses}>
+        <label htmlFor="city">City</label>
+        <input
+          type="text"
+          id="city"
+          value={cityValue}
+          onChange={cityChangeHandler}
+          onBlur={cityBlurHandler}
+        />
+        {cityHasError && (
+          <p className={classes.invalidtext}>Please enter a valid city!</p>
+        )}
+      </div>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit} disabled={!formIsValid}>
+          Confirm
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Checkout;
---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,70 @@
+import { useState, useEffect } from 'react';
+
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import classes from './AvailableMeals.module.css';
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      setIsLoading(true);
+
+      const response = await fetch(
+        'https://react-food-order-app-alireza-default-rtdb.firebaseio.com/meals.json'
+      );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  const mealsList = meals.map((meal) => (
+    <li>
+      <MealItem
+        key={meal.id}
+        id={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    </li>
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+        {isLoading && <p className={classes.loading}>Loading...</p>}
+        {httpError && <p className={classes.error}>{httpError}</p>}
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
