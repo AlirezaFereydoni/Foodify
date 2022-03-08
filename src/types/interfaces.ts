c6bce1b24fc9ad13dbaf8fb0ae089986dc699246
Alireza Fereydoni <AlirezaFereydoni@Outlook.CoM>
2022-03-08 22:00:04 +0330

    Revert "Start to migrating code base into Ts "

--- src/types/interfaces.ts
+++
@@ -1,7 +0,0 @@
-export interface iCart {
-  items: { [key: string]: string | number }[];
-  totalAmount: number;
-  addItem: (item: any) => void;
-  removeItem: (id: number) => void;
-  clearCart: () => void;
-}
