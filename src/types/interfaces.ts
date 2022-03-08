69c6f71e6030b7440f45f43c2e1957de26fe508f
Alireza Fereydoni <Aliirzw@gmail.com>
2022-03-08 23:31:39 +0330

    Add some usefull type

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,7 +1,21 @@
 export interface iCart {
-  items: { [key: string]: string | number }[];
+  items: itemType[];
   totalAmount: number;
-  addItem: (item: any) => void;
+  addItem: (item: itemType) => void;
   removeItem: (id: number) => void;
   clearCart: () => void;
+}
+
+export interface itemType {
+  id: number;
+  name: string;
+  price: number;
+  amount: number;
+}
+
+export interface iConfirm {
+  name: string;
+  street: string;
+  city: string;
+  postalCode: string;
 }
