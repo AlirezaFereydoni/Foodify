6c6ebfab096ab1c0f2ca15831b4622a66e61a688
Alireza Fereydoni <Aliirzw@gmail.com>
2022-03-12 20:45:50 +0330

    Change modal component into Ts

---
+++ src/components/UI/Modal.tsx
@@ -0,0 +1,42 @@
+import { Children, Fragment, ReactNode } from "react";
+import ReactDom from "react-dom";
+
+import classes from "./Modal.module.css";
+
+interface iBackDrop {
+  onClick: () => void;
+}
+
+interface iModalOverLay {
+  children: ReactNode;
+}
+
+interface iModal {
+  onCloseCart: () => void;
+  children: ReactNode;
+}
+
+const Backdrop = (props: iBackDrop) => {
+  return <div className={classes.backdrop} onClick={props.onClick} />;
+};
+
+const ModalOverlay = (props: iModalOverLay) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays")!;
+
+const Modal = (props: iModal) => {
+  return (
+    <Fragment>
+      {ReactDom.createPortal(<Backdrop onClick={props.onCloseCart} />, portalElement)}
+      {ReactDom.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+    </Fragment>
+  );
+};
+
+export default Modal;
